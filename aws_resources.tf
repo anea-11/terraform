f154fd9ba08f2347ff46cd75e874a37b6e90a577
Andrijana <andrijana.ojdanic@tutanota.com>
2023-07-13 23:33:25 +0200

    Add some tags to resources

--- aws_resources.tf
+++ aws_resources.tf
@@ -6,12 +6,19 @@
 
 resource "aws_vpc" "development-vpc" {          # here you declare that you want a vpc
     cidr_block = "10.0.0.0/16"
+    tags = {
+        Name: "development",                    # name is a built-in tag type
+        vpc_env: "dev"                          # this is a completely custom key:value pair
+    }
 }
 
 resource "aws_subnet" "dev-subnet-1" {          # here you declare that you want a subnet
     vpc_id = aws_vpc.development-vpc.id         # a subnet is created inside of a vpc; this is how you access a vpc that is created in you .tf files
     cidr_block = "10.0.10.0/24"
     availability_zone = "eu-west-3a"
+    tags = {
+        Name: "subnet-1-dev"
+    }
 }
 
 data "aws_vpc" "existing-vpc" {                 # here you retrieve an existing resource (vpc); by existing, I mean that it's not created by this .tf repo
@@ -23,4 +30,7 @@ resource "aws_subnet" "dev-subnet-2" {          # here you declare that you want
     vpc_id = data.aws_vpc.existing-vpc.id       # this is how you access a resource that is retrieved as "data"
     cidr_block = "172.31.64.0/20"
     availability_zone = "eu-west-3a"
+    tags = {
+        Name: "subnet-2-dev"
+    }
 }
