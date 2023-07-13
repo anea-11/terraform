489c79698652f94e38f5c5b2899a70af62592d97
Andrijana <andrijana.ojdanic@tutanota.com>
2023-07-14 01:43:41 +0200

    Add info about terraform state

--- main.tf
+++ main.tf
@@ -12,12 +12,19 @@ terraform {
     }
 }
 
-/*
-Useful Terraform commands
+/* Useful Terraform commands
 1.  terraform plan
     - preview: shows you the difference between current state and desired state (described in .tf files); similar to terrafor apply, but without actually applying
 2. terraform apply
     - apply the current configuration (desired state) to the infrastructure
 3. terraform destroy (without specifying any specific resource)
     - destroys everything that's defined in .tf files
+4. terraform state
+    - info about terraform state
+*/
+
+/* Terraform state
+- Stored in file terraform.tfstate (JSON); shows the actual state of the infrastructure
+- terraform state <subcommand> can be used to slice and dice the state file, to get whatever info you may need from it
+- terraform state show ---> to see the list of all resources
 */
