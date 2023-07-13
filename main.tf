e85667e29657f69056ec325127261277f79990f3
Andrijana <andrijana.ojdanic@tutanota.com>
2023-07-13 23:43:18 +0200

    Add info about useful terraform commands

--- main.tf
+++ main.tf
@@ -11,3 +11,13 @@ terraform {
       }
     }
 }
+
+/*
+Useful Terraform commands
+1.  terraform plan
+    - preview: shows you the difference between current state and desired state (described in .tf files); similar to terrafor apply, but without actually applying
+2. terraform apply
+    - apply the current configuration (desired state) to the infrastructure
+3. terraform destroy (without specifying any specific resource)
+    - destroys everything that's defined in .tf files
+*/
