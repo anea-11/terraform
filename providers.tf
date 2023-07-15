07cb9352f2ae3f2d15f70dfab39f8b920d8d112e
Andrijana <andrijana.ojdanic@tutanota.com>
2023-07-15 18:54:47 +0200

    Pass secret info (aws credentials) as env vars

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws"{
     region = "eu-west-3"
-    access_key = "AKIAW4XQPHIZKRBAQCRK"                     # bad practice, never commit this in plaintext, demo purpose only
-    secret_key = "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" # bad practice, never commit this in plaintext, demo purpose only
+    # export AWS_ACCESS_KEY_ID=value            good practice, pass secret info as env vars
+    # export AWS_SECRET_ACCESS_KEY=value        good practice, pass secret info as env vars
 }
